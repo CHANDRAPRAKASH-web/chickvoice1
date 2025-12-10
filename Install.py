2bdc92430be2409ea977be9afefdb448ebcae6f1
CHANDRAPRAKASH <chandraprakash.manikandan@gmail.com>
2025-12-10 09:31:16 +0530

    Create initial source files in src directory

---
+++ Install.py
@@ -0,0 +1,5 @@
+New-Item src/__init__.py -ItemType File
+New-Item src/dataset.py -ItemType File
+New-Item src/model.py -ItemType File
+New-Item src/train.py -ItemType File
+New-Item src/infer.py -ItemType File
